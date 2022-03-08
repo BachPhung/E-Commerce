16398ee0d29a747cd1e9ea78f5d2fd9f0cad29d6
Bach Phung <quangbach19112000@gmail.com>
2022-03-09 01:39:35 +0200

    Push

--- client/src/components/preView/preView.jsx
+++ client/src/components/preView/preView.jsx
@@ -3,11 +3,11 @@ import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/
 import app from '../../redux/firebase'
 import { addProduct } from '../../redux/cartRedux';
 import { useDispatch } from 'react-redux';
-import { Avatar } from '@material-ui/core';
+import { Avatar, Input, TextField, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
+import './preView.scss'
 const PreView = () => {
     const dispatch = useDispatch()
     const [avatar, setAvatar] = useState()
-    const [file, setFile] = useState(null)
     const [inputs, setInputs] = useState({})
     const [cat, setCat] = useState([])
     useEffect(() => {
@@ -69,21 +69,117 @@ const PreView = () => {
     }
     return (
         <div>
-            <h1>Hello</h1>
-            <form onSubmit={handleClick}>
-                <label>Upload avatar: </label>
-                <input type='file' onChange={handlePreviewAvatar} />
-                <label>Title: </label>
-                <input name='title' onChange={handleChange} type='text' />
-                <label>Desc: </label>
-                <input name='desc' onChange={handleChange} type='text' />
-                {/* {avatar && (<img src={avatar.preview} alt='preview-img' width='80%' />)} */}
+            <h1>Add Product </h1>
+            <div className='wrapper'>
+                <form className='form' onSubmit={handleClick}>
+                    <label htmlFor="contained-button-file">
+                        <Input style={{ display: 'none' }} accept="image/*" id="contained-button-file" type="file" onChange={handlePreviewAvatar} required />
+                        <Button variant="outlined" component="span">
+                            Product Image
+                        </Button>
+                    </label>
+
+                    <TextField
+                        className='input-element'
+                        label="Size"
+                        id="filled-size-small"
+                        placeholder="XS, S, ..."
+                        variant="filled"
+                        size="small"
+                        name='title'
+                        onChange={handleChange}
+                        type='text'
+                        variant="standard"
+                        required
+                    />
+                    <TextField
+                        className='input-element'
+                        label="Desc"
+                        id="filled-size-small"
+                        placeholder="Description"
+                        variant="filled"
+                        size="small"
+                        name='title'
+                        onChange={handleChange}
+                        type='text'
+                        variant="standard"
+                        required
+                    />
+                    <TextField
+                        className='input-element'
+                        label="Category"
+                        id="filled-size-small"
+                        placeholder="T-shirt, Women, ..."
+                        variant="filled"
+                        size="small"
+                        name='title'
+                        onChange={handleChange}
+                        type='text'
+                        variant="standard"
+                        required
+                    />
+                    <TextField
+                        className='input-element'
+                        label="Color"
+                        id="filled-size-small"
+                        placeholder="Yellow, Red, ..."
+                        variant="filled"
+                        size="small"
+                        name='title'
+                        onChange={handleChange}
+                        type='text'
+                        variant="standard"
+                        required
+                    />
+                    <TextField
+                        className='input-element'
+                        label="Price"
+                        id="filled-size-small"
+                        placeholder="Number"
+                        variant="filled"
+                        size="small"
+                        name='title'
+                        onChange={handleChange}
+                        type='number'
+                        variant="standard"
+                        required
+                    />
+                    <TextField
+                        className='input-element'
+                        label="Desc"
+                        id="filled-size-small"
+                        placeholder="Description"
+                        variant="filled"
+                        size="small"
+                        name='title'
+                        onChange={handleChange}
+                        type='text'
+                        variant="standard"
+                        required
+                    />
+                    <FormControl className='input-element' variant="standard" sx={{ minWidth: 150 }}>
+                        <InputLabel style={{ width: '200px' }} id="demo-simple-select-standard-label" required>In Stock</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-standard-label"
+                            id="demo-simple-select-standard"
+                            name='inStock'
+                            onChange={handleChange}
+                            label="In Stock"
+                            style={{ width: '185px' }}
+                        >
+                            <MenuItem value={true}>Yes</MenuItem>
+                            <MenuItem value={false}>No</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <button type='submit' className='addProductButton'>Upload</button>
+                </form>
+                <div className='right'>
                 {avatar && <Avatar
                     src={avatar.preview}
                     style={{ height: '500px', width: '500px' }}
                 />}
-                <button type='submit' className='addProductButton'>Create</button>
-            </form>
+                </div>
+            </div>
         </div>
     )
 }
